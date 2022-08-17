4a270a856e466513680b5383f28c85115ef9fc83
antrakt92 <98484916+Antrakt92@users.noreply.github.com>
2022-08-17 18:24:37 +0300

    upl

---
+++ js/app.js
@@ -0,0 +1,34 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    document.addEventListener("click", (function(e) {
+        const targetElement = e.target;
+        if (targetElement.closest(`.skill-block__body`)) {
+            targetElement.closest(`.skill-block__body`).classList.toggle(`-active`);
+            e.preventDefault();
+        }
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
